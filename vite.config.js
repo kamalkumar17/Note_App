c44ad733aead91b76d08710d0179d9af5fcb5af8
kamal kumar <kamalkumar1752000@gmail.com>
2025-04-30 19:17:48 +0530

    make vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, // Add this line for development
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
